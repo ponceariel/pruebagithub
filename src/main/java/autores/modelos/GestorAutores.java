45e147b397a52519035122024c4a37b740f5009a
Facundo Suarez <willz4321@gmail.com>
2020-11-09 20:02:20 -0300

    se avanza con gestorAutor

--- src/main/java/autores/modelos/GestorAutores.java
+++ src/main/java/autores/modelos/GestorAutores.java
@@ -18,6 +18,8 @@ public class GestorAutores implements IGestorAutores {
     public static final String Descripcion_Cambio="se cambio la descipcion";
     
     private ArrayList<Autor> AUTOR= new ArrayList<>();
+    private ArrayList<Profesor> PROFESORES= new ArrayList<>();
+    private ArrayList<Alumno> ALUMNOS= new ArrayList<>();
     private static GestorAutores auxiliar;
     
     public static GestorAutores crear(){
@@ -31,7 +33,8 @@ public class GestorAutores implements IGestorAutores {
         if((dni!=0)&&(apellidos!=null)&&(!apellidos.isBlank())&&(nombres!=null)&&(!nombres.isBlank())&&(cargo!=null)&&(claveRepetida!=null)&&(!claveRepetida.isBlank())){
             Autor profe=new Profesor(dni,apellidos,nombres,clave,cargo);
             
-            if(!this.AUTOR.contains(profe)){
+            if(!this.PROFESORES.contains(profe) && !this.AUTOR.contains(profe)){
+                this.PROFESORES.add((Profesor) profe);
                 this.AUTOR.add(profe);
                 return Autor_Correcto;
             }
@@ -45,8 +48,9 @@ public class GestorAutores implements IGestorAutores {
        if((dni!=0)&&(apellidos!=null)&&(!apellidos.isBlank())&&(nombres!=null)&&(!nombres.isBlank())&&(cx!=0)&&(claveRepetida!=null)&&(!claveRepetida.isBlank())){
             Autor alumno=new Alumno(dni,apellidos,nombres,clave,cx);
             
-            if(!this.AUTOR.contains(alumno)){
+            if(!this.AUTOR.contains(alumno) && !this.ALUMNOS.contains(alumno)){
                 this.AUTOR.add(alumno);
+                this.ALUMNOS.add((Alumno) alumno);
                 return Autor_Correcto;
             }
              else{
@@ -68,43 +72,82 @@ public class GestorAutores implements IGestorAutores {
                 this.AUTOR.get(j).setClave(claveRepetida);
                 
             } 
-       }
+       } 
+            for(int i=0;i<PROFESORES.size(); i++)
+          {
+                if(this.PROFESORES.contains(autor)){
+                this.PROFESORES.get(i).setApellidos(apellidos);
+                this.PROFESORES.get(i).setNombres(nombres);
+                this.PROFESORES.get(i).setClave(clave);
+                this.PROFESORES.get(i).setClave(claveRepetida);
+                
+            } 
      }
-       return Autor_Correcto;
-   }
+      
+   }  return Autor_Correcto; 
+ }
     @Override
     public String modificarAutor(Autor autor, String apellidos, String nombres, String cx, String clave, String claveRepetida) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
+      if((apellidos!=null)&&(!apellidos.isBlank())&&(nombres!=null)&&(!nombres.isBlank())&&(cx!=null)&&(claveRepetida!=null)&&(!claveRepetida.isBlank())){
+            for(int j=0;j<AUTOR.size(); j++)
+       {
+            if(this.AUTOR.contains(autor)){
+                this.AUTOR.get(j).setApellidos(apellidos);
+                this.AUTOR.get(j).setNombres(nombres);
+                this.AUTOR.get(j).setClave(clave);
+                this.AUTOR.get(j).setClave(claveRepetida);
+                
+            }
+              for(int i=0;i<ALUMNOS.size(); i++)
+          {
+                if(this.ALUMNOS.contains(autor)){
+                this.ALUMNOS.get(i).setApellidos(apellidos);
+                this.ALUMNOS.get(i).setNombres(nombres);
+                this.ALUMNOS.get(i).setClave(clave);
+                this.ALUMNOS.get(i).setClave(claveRepetida);
+                
+            } 
+       }
+     }
+       
+    }return Autor_Correcto;  
+  }
     @Override
     public boolean existeEsteAutor(Autor autor) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean b=false;
+     for(int i=0;i<AUTOR.size();i++){
+         if(AUTOR.get(i).equals(autor)){
+             b=true;
+         }
+     }
+     return b;
     }
 
     @Override
     public ArrayList<Autor> verAutores() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       return AUTOR;
     }
 
     @Override
     public ArrayList<Profesor> verProfesores() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       return PROFESORES;
     }
 
     @Override
     public ArrayList<Alumno> verAlumnos() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       return ALUMNOS;
     }
 
     @Override
     public Autor verAutor(int dni) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Autor Avalor=null;    
+        for(int i=0;i<AUTOR.size();i++)
+        {
+            if(AUTOR.get(i).getDNI()==dni)
+            {
+                Avalor=AUTOR.get(i);
+            }
+        }
+        return Avalor;
     }
-
-    @Override
-    public String nuevoAutor(int dni, String apellidos, String nombres, String cx, String clave, String claveRepetida) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
 }
